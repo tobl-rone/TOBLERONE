d8222665d8fd0a7506b59909d731363a6afbe8d2
Emmeline VOURIOT <vouriote@iut-acy.local>
2019-06-04 13:49:49 +0200

    fonctions pour l'enigmes des couleurs
    script_plaque_couleurs.cs  gère les plaques de couleur. Quand on marche dessus, elles changent de texture. Les deux textures doivent être données en paramètre.
    reset_plaque_couleur.cs gère les plaques dynamiques. Fonctionne grace au nom de fonction passé en paramètre:
    Reset : remet toutes les plaques de couleurs à leur texture par default. Nécessite que les plaques de couleurs aient les bonnes étiquettes.
    Fin_enigme_couleur : à la sortie de l'enigme, si toutes les plaques sont à la bonne couleur, active un mécanisme (ici change de couleur un ploc avec l'étiquette porte). nécessite les étiquettes adéquates sur les plaques de couleurs

---
+++ Fonctions pour enigmes/reset_plaque_couleur.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reset_plaque_couleur : MonoBehaviour {
+
+    enum Fonction { Reset, Fin_enigme_couleurs }
+    public Renderer rend;
+    [SerializeField] string NomFonction;
+    [SerializeField] Texture defaulttexture;
+    [SerializeField] Texture redtext;
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (NomFonction)
+        {
+            case "Reset":
+                Reset();
+                break;
+            case "Fin_enigme_couleurs":
+                Fin_enigme_couleurs();
+                break;
+            default:
+                Debug.Log("Fonction donnée inconnue");
+                break;
+        }
+    }
+
+    private void Reset()
+    {
+        foreach (GameObject ObjectFound in GameObject.FindGameObjectsWithTag("plaque_couleur"))
+        {
+            //Do something to ObjectFound, like this:
+            ObjectFound.GetComponent<Renderer>().material.mainTexture = defaulttexture;
+            //rend.material.mainTexture = texture;
+        }
+    }
+
+    private void Fin_enigme_couleurs()
+    {
+
+        bool check = true;
+        foreach (GameObject ObjectFound in GameObject.FindGameObjectsWithTag("plaque_couleur"))
+        {
+            //Do something to ObjectFound, like this:
+            if (ObjectFound.GetComponent<Renderer>().material.mainTexture != redtext)
+            {
+                check = false;
+                break;
+            }
+            //rend.material.mainTexture = texture;
+        }
+        if (check)
+        {
+
+            var truc = GameObject.FindGameObjectWithTag("porte_fermee");
+
+            var rend = truc.GetComponent<Renderer>();
+            rend.material.mainTexture = redtext;
+            foreach (GameObject ObjectFound in GameObject.FindGameObjectsWithTag("plaque_couleur"))
+            {
+                Destroy(ObjectFound);
+            }
+        }
+    }
+}
---
+++ Fonctions pour enigmes/script_plaque_couleur.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_plaque_couleur : MonoBehaviour
+{
+
+
+    //public Color color;
+    public Renderer rend;
+    [SerializeField] Texture blutext;
+    [SerializeField] Texture redtext;
+    // Use this for initialization
+    void Start()
+    {
+        //color = Color.blue;
+        rend = GetComponent<Renderer>();
+        rend.material.mainTexture = blutext;
+        //rend.material.color = color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rend.material.color = color;
+        //rend.material.mainTexture = texture;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (rend.material.mainTexture.Equals(redtext))
+        {
+            rend.material.mainTexture = blutext;
+        }
+        else
+        {
+            rend.material.mainTexture = redtext;
+        }
+    }
+}
